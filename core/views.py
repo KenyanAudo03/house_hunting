a9953e9d400b20929459ad0de7d1e51a20662a48
Victor Owino <vo4685336@gmail.com>
2025-09-02 18:30:38 +0300

    Add About page URL and view

--- core/views.py
+++ core/views.py
@@ -2,23 +2,30 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Hostel
 
+
 def home(request):
     hostels = Hostel.objects.all()
-    return render(request, 'core/home.html', {'hostels': hostels})
+    return render(request, "core/home.html", {"hostels": hostels})
+
 
 def search(request):
-    query = request.GET.get('q', '')
+    query = request.GET.get("q", "")
     hostels = Hostel.objects.filter(name__icontains=query)
     data = {
-        'hostels': [
+        "hostels": [
             {
-                'id': h.id,
-                'name': h.name,
-                'address': h.address,
-                'pricing': str(h.pricing),
-                'available_vacants': h.available_vacants,
-                'average_rating': round(h.average_rating, 1),
-            } for h in hostels
+                "id": h.id,
+                "name": h.name,
+                "address": h.address,
+                "pricing": str(h.pricing),
+                "available_vacants": h.available_vacants,
+                "average_rating": round(h.average_rating, 1),
+            }
+            for h in hostels
         ]
     }
     return JsonResponse(data)
+
+
+def about(request):
+    return render(request, "core/about.html")
