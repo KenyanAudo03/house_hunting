438ff1cb23880722819a9621baaa2722c79e7cba
Victor Owino <vo4685336@gmail.com>
2025-09-09 02:45:29 +0300

    Remove unused search path from URL patterns

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("search/", views.search, name="search"),
     path("about", views.about, name="about"),
     path("contact", views.contact, name="contact"),
     path("privacy_policies", views.privacy_policy, name="privacy_policy"),
