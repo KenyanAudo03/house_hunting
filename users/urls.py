4fff414e3532b9f3a0e888632e79df532546fa09
Victor Owino <vo4685336@gmail.com>
2025-09-15 11:27:00 +0300

    Remove trailing slashes from URL paths

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("dashboard/", views.dashboard, name="dashboard"),
-    path("profile/", views.profile, name="profile"),
-    path("favorites/", views.favorites, name="favorites"),
-    path("roomie-profile/", views.roomie_profile, name="roomie_profile"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("profile", views.profile, name="profile"),
+    path("favorites", views.favorites, name="favorites"),
+    path("roomie-profile", views.roomie_profile, name="roomie_profile"),
 ]
