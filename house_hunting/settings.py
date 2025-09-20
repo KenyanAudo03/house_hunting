1f37eb78214cc834dc7d205163615ccf4ced2ac6
Victor Owino <vo4685336@gmail.com>
2025-09-20 22:31:45 +0300

    config: register favorites context processor in settings

--- house_hunting/settings.py
+++ house_hunting/settings.py
@@ -112,6 +112,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "contact.context_processors.platform_contact",
+                "accounts.context_processors.favorites_count",
             ],
         },
     },
