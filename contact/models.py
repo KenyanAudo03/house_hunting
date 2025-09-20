d0fa77f33ebd809d41e86581b9fe2bd5c550201e
Victor Owino <vo4685336@gmail.com>
2025-09-20 13:32:12 +0300

    Add PlatformContact model for storing platform emails and contact number

--- contact/models.py
+++ contact/models.py
@@ -26,3 +26,16 @@ class ContactInquiry(models.Model):
 
     def __str__(self):
         return f"Inquiry from {self.first_name} {self.last_name} ({self.email})"
+
+
+class PlatformContact(models.Model):
+    contact_number = models.CharField(max_length=20, blank=True, null=True)
+    support_email = models.EmailField(blank=True, null=True)
+    partners_email = models.EmailField(blank=True, null=True)
+    issues_email = models.EmailField(blank=True, null=True)
+    privacy_email = models.EmailField(blank=True, null=True)
+    terms_email = models.EmailField(blank=True, null=True)
+    cookie_email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return "Platform Contact Information"
