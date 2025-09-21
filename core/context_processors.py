38b734f04a42ebf2a094c312c876bf3ba3c2ae03
Victor Owino <vo4685336@gmail.com>
2025-09-21 03:06:53 +0300

    add google_client_id context processor for all templates

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,10 @@
 from .models import PropertyListing
+import os
+
+def google_client_id(request):
+    return {
+        "google_client_id": os.environ.get("GOOGLE_CLIENT_ID", "")
+    }
 
 
 def user_listings_count(request):
